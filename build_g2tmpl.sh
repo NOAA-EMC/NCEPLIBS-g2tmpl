4807f1c3edb6d63c555461816b56feaf1b7f22eb
Dexin.Zhang <dexin.zhang@noaa.gov>
2019-06-18 15:20:28 +0000

    Unified build (20190618) script/makefile g2tmpl v1.3.0

--- build_g2tmpl.sh
+++ build_g2tmpl.sh
@@ -1,37 +1,6 @@
 #!/bin/bash
 
- (( $# == 0 )) && {
-   echo "*** Usage: $0 wcoss|dell|cray|theia|intel_general|gnu_general [debug|build] [[local]install[only]]" >&2
-   exit 1
- }
-
- sys=${1,,}
- [[ $sys == wcoss || $sys == dell || $sys == cray ||\
-    $sys == theia || $sys == intel_general || $sys == gnu_general ]] || {
-   echo "*** Usage: $0 wcoss|dell|cray|theia|intel_general|gnu_general [debug|build] [[local]install[only]]" >&2
-   exit 1
- }
- debg=false
- inst=false
- skip=false
- local=false
- (( $# > 1 )) && {
-   [[ ${2,,} == build ]] && debg=false
-   [[ ${2,,} == debug ]] && debg=true
-   [[ ${2,,} == install ]] && inst=true
-   [[ ${2,,} == localinstall ]] && { local=true; inst=true; }
-   [[ ${2,,} == installonly ]] && { inst=true; skip=true; }
-   [[ ${2,,} == localinstallonly ]] && { local=true; inst=true; skip=true; }
- }
- (( $# > 2 )) && {
-   [[ ${3,,} == build ]] && debg=false
-   [[ ${3,,} == debug ]] && debg=true
-   [[ ${3,,} == install ]] && inst=true
-   [[ ${3,,} == localinstall ]] && { local=true; inst=true; }
-   [[ ${3,,} == installonly ]] && { inst=true; skip=true; }
-   [[ ${3,,} == localinstallonly ]] && { local=true; inst=true; skip=true; }
- }
-
+ source ./Conf/Analyse_args.sh
  source ./Conf/Collect_info.sh
  source ./Conf/Gen_cfunction.sh
  source ./Conf/Reset_version.sh
@@ -84,19 +53,27 @@ set -x
 #     Install libraries and source files
 #
    $local && {
-              LIB_DIR=..
-              INCP_DIR=../include
-              [ -d $INCP_DIR ] || { mkdir -p $INCP_DIR; }
-              SRC_DIR=
-             } || {
-              LIB_DIR=$(dirname $G2TMPL_LIB)
-              INCP_DIR=$(dirname $G2TMPL_INC)
-              SRC_DIR=$G2TMPL_SRC
-              [ -d $LIB_DIR ] || mkdir -p $LIB_DIR
-              [ -d $G2TMPL_INC ] && { rm -rf $G2TMPL_INC; } \
-                                 || { mkdir -p $INCP_DIR; }
-              [ -z $SRC_DIR ] || { [ -d $SRC_DIR ] || mkdir -p $SRC_DIR; }
-             }
+     instloc=..
+     LIB_DIR=$instloc
+     INCP_DIR=$instloc/include
+     [ -d $INCP_DIR ] || { mkdir -p $INCP_DIR; }
+     SRC_DIR=
+   } || {
+     [[ $instloc == --- ]] && {
+       LIB_DIR=$(dirname $G2TMPL_LIB)
+       INCP_DIR=$(dirname $G2TMPL_INC)
+       SRC_DIR=$G2TMPL_SRC
+     } || {
+       LIB_DIR=$instloc
+       INCP_DIR=$instloc/include
+       SRC_DIR=$instloc/src
+       [[ $instloc == .. ]] && SRC_DIR=
+     }
+     [ -d $LIB_DIR ] || mkdir -p $LIB_DIR
+     [ -d $G2TMPL_INC ] && { rm -rf $G2TMPL_INC; } \
+                        || { mkdir -p $INCP_DIR; }
+     [ -z $SRC_DIR ] || { [ -d $SRC_DIR ] || mkdir -p $SRC_DIR; }
+   }
 
    make clean LIB=
    make install LIB=$g2tmplLib MOD=$g2tmplInc \
