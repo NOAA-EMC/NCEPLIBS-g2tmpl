61412eabd10b0ab2d7820674b288fe368ce4b99a
Edward Hartnett <edward.hartnett@noaa.gov>
2022-08-23 15:13:14 -0600

    adding new test file

--- tests/test_all_table_other.f90
+++ tests/test_all_table_other.f90
@@ -8,8 +8,9 @@ program test_all_table_other
   implicit none
 
   integer :: idisc, listsec0(2)
-
   integer :: listsec1(13), i
+  integer :: ipdstmpl0(15)
+  integer :: ipdstmpl0_expected(15) = (/0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0/)
 
   print *, 'Testing grib2_all_tables_module, expect and ignore error messages...'
   print *, 'testing g2sec0'
@@ -25,6 +26,14 @@ program test_all_table_other
   if (listsec1(6) .ne. 2022 .or. listsec1(7) .ne. 8 .or. listsec1(8) .ne. 23) stop 2
   if (listsec1(9) .ne. 14 .or. listsec1(10) .ne. 45 .or. listsec1(11) .ne. 0) stop 2
   if (listsec1(12) .ne. 0 .or. listsec1(13) .ne. 0) stop 2
+
+  print *, 'testing g2sec4_temp0'
+  call g2sec4_temp0(0, 0, 'anal', 'res', 0, 0, 'minute', 0, 'surface', 0, &
+       0, 'surface', 0, 0, ipdstmpl0)
+  do i = 1, 15
+     if (ipdstmpl0(i) .ne. ipdstmpl0_expected(i)) stop 3
+  end do
+    
   
   print *, 'SUCCESS!!'
 end program test_all_table_other
